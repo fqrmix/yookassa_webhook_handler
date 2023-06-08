1265ee87037cfd2d9098ba6e9ac97af30769b077
Semen Sergeev <fqrmix@macbook-air.local>
2023-06-08 09:53:42 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request, Response, status
+
+app = FastAPI()
+
+@app.post("/yookassa/webhook/apiv3/error_example")
+async def handle_error(request: Request, response: Response):
+    headers = request.headers
+    payload = await request.json()
+    print(f"Headers:\n{headers}\n\nBody:\n{payload}")
+    response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+
+@app.post("/yookassa/webhook/apiv3/success_example")
+async def handle_success(request: Request, response: Response):
+    headers = request.headers
+    payload = await request.json()
+    print(f"Headers:\n{headers}\n\nBody:\n{payload}")
+    response.status_code = status.HTTP_200_OK
