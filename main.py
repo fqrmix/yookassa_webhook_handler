b67b9fa77b78418b162cb884f1beff68e2686a6d
Semen Sergeev <fqrmix@macbook-air.local>
2023-06-08 10:09:24 +0300

    Handle json error

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, Request, Response, status
+from json import JSONDecodeError
 
 app = FastAPI()
 
 @app.post("/webhook/apiv3/error_example")
 async def handle_error(request: Request, response: Response):
     headers = request.headers
-    payload = await request.json()
+    try:
+        payload = await request.json()
+    except JSONDecodeError:
+        payload = await request.body()
     print(f"Headers:\n{headers}\n\nBody:\n{payload}")
     response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
 
@@ -13,6 +17,9 @@ async def handle_error(request: Request, response: Response):
 @app.post("/webhook/apiv3/success_example")
 async def handle_success(request: Request, response: Response):
     headers = request.headers
-    payload = await request.json()
+    try:
+        payload = await request.json()
+    except JSONDecodeError:
+        payload = await request.body()
     print(f"Headers:\n{headers}\n\nBody:\n{payload}")
     response.status_code = status.HTTP_200_OK
